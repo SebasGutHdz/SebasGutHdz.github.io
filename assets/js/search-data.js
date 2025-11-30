79a164f0173a96af019b5a4ffa515ce0f74cbe89
SebasGutHdz <30961060+SebasGutHdz@users.noreply.github.com>
2025-11-30 22:16:31 +0000

    Deploying to gh-pages from @ SebasGutHdz/SebasGutHdz.github.io@379d6ea0614668493160e44700b499c423ff8cf9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here you can find the hihglights of my CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
